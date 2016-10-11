07c01119201fbeab503658279746c5ce30d8db7e
Eduardo Lopez <eddielopez2101@gmail.com>
2016-10-11 11:49:35 -0500

    all test files have imports and ready for tests

---
+++ __tests__/home-test.js
@@ -0,0 +1,10 @@
+import React form 'react'
+import ReactDOM from 'react-dom'
+import TestUtils from 'react-addons-test-utils'
+import Home from '../lib/Home'
+
+describe("Home", () => {
+  it("should be a passing test for setup", () => {
+    expect(true).toBe(true)
+  })
+})
---
+++ __tests__/login-test.js
@@ -0,0 +1,10 @@
+import React form 'react'
+import ReactDOM from 'react-dom'
+import TestUtils from 'react-addons-test-utils'
+import Login from '../lib/Login'
+
+describe("Login", () => {
+  it("should be a passing test for setup", () => {
+    expect(true).toBe(true)
+  })
+})
