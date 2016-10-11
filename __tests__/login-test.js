14b67cc2f1c52c306aff8b2846038bdc2c0de6d5
Eduardo Lopez <eddielopez2101@gmail.com>
2016-10-11 14:14:51 -0500

    test files now ready for tests

--- __tests__/login-test.js
+++ __tests__/login-test.js
@@ -4,7 +4,23 @@ import TestUtils from 'react-addons-test-utils'
 import Login from '../lib/Login'
 
 describe("Login", () => {
+  beforeEach(()=>{
+    window.firebase = {
+    database: ()=>{
+      return {
+        ref: ()=>{
+          return {
+            once: ()=>{
+
+            }
+          }
+        }
+      }
+    }
+  }
+  })
   it("should be a passing test for setup", () => {
+
     expect(true).toBe(true)
   })
 })
